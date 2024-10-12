66e23690c70aeb956a3c9c173717babe92e85112
nguyenvantien2403 <88313441+nguyenvantien2403@users.noreply.github.com>
2024-10-12 22:54:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Đổi dataconnection ở appseting.json về db máy local
+Database migration bằng 
+Update-database
+
+
+
+chạy với iis để protolcol là https
---
+++ Sale.Service/ProductService/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using Sale.Domain.Entities;
+using Sale.Service.Common;
+using Sale.Service.Core;
+using Sale.Service.Dtos.ProductDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sale.Service.ProductService
+{
+	public interface IProductService : IService<Product>
+	{
+		Task<PageList<ProductDto>> GetDataByPage(ProductSearchDto searchDto);
+		ProductDetailDto? FindDetailProduct(Guid id);
+		List<ProductDto>? GetBestSaleProduct();
+        int? countProduct(Guid ProductId);
+
+
+		bool UpdateCountSold(Guid ProductId,int count);
+        bool UpdateFaildCountSold(Guid ProductId, int count);
+    }
+}
